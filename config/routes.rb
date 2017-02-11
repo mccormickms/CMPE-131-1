470f5157bc544dad8b74d28026fef3981f63d820
Matt McCormick <mccormick.ms@gmail.com>
2017-02-11 10:32:13 -0800

    Add Hello static page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
+  root 'application#hello'
+
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
