c8c89b13f5c168f46ed95c381275d664e4e0adcb
Matt McCormick <mccormick.ms@gmail.com>
2017-05-08 21:36:19 -0700

    start login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
 	root 'static_pages#home' #sets home page automagically, skipping the "Yay, Rails" splash page
 
   get '/help', 			to: 'static_pages#help'
@@ -10,7 +12,11 @@ Rails.application.routes.draw do
 
   get '/signup',    to: 'users#new'
 
-  post '/signup',   to: 'users#create'
+  get '/login',     to: 'sessions#new'
+
+  post '/login',    to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   get 'users/new'
 
